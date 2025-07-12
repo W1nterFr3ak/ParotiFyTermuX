2cfdfb083fc52ec506000b91f817444d636c7ec2
Winter <WinterFreak@protonmail.com>
2025-07-12 10:07:03 +0300

    Update installer.py
    v 0.3 tricetech - fixed installer

--- installer.py
+++ installer.py
@@ -1,2 +1,2 @@
 import os
-os.system("pkg update && pkg upgrade && pkg install toilet ruby && gem install lolcat")
+os.system("pkg update && pkg upgrade && pkg install toilet toilet-tools ruby && gem install lolcat")
